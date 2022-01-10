33d0ac4d914975b0560b673c61f4d726c4f71742
Chris Masters <chrismasters@gmail.com>
2022-01-10 19:15:24 +0100

    formats date into french (if necessary)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { format } from 'date-fns'
+import fr from 'date-fns/locale/fr/index.js'
+
 const args = process.argv.slice(2)
 
-console.log(args)
+let inputDate = args.join(' ')
+
+
+
+
+const jsDate = new Date(inputDate)
+
+const formattedDate = format(jsDate, 'd MMM yyyy', {locale: fr})
+console.log(formattedDate)
